06048bb59b8d3d074dfc65f14bec960caec0f943
xl-ding <65400806+xl-ding@users.noreply.github.com>
2020-09-21 00:49:21 +0800

    Add files via upload

---
+++ src/CeChat.App/CeChatApp.cs
@@ -0,0 +1,24 @@
+﻿using CeChat.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CeChat.App
+{
+    public partial class CeChatApp : Form
+    {
+        public ICeChatRoomService CeChatRoomService { get; }
+
+        public CeChatApp(ICeChatRoomService ceChatRoomService)
+        {
+            InitializeComponent();
+            this.CeChatRoomService = ceChatRoomService;
+        }
+    }
+}
